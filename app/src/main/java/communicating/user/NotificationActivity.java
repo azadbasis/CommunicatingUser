a2646e4d1d7c7bec1072ded5b358dcaa74671de5
Azhar <azadbasis@gmail.com>
2019-02-24 23:56:22 +0600

    ExpandedNotification

--- app/src/main/java/communicating/user/NotificationActivity.java
+++ app/src/main/java/communicating/user/NotificationActivity.java
@@ -29,6 +29,7 @@ public class NotificationActivity extends AppCompatActivity
         // TODO: Create the intent that will start the ResultActivity when the user
         // taps the notification or chooses an action button
         Intent intent = new Intent(this, NotificationResultActivity.class);
+        intent.putExtra("notifyID",NOTIFY_ID);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, NOTIFY_ID, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         // Store the notification ID so we can cancel it later in the ResultActivity
@@ -42,28 +43,33 @@ public class NotificationActivity extends AppCompatActivity
         builder.setAutoCancel(true);
 
         // TODO: Set the large icon to be our app's launcher icon
-        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
 
         // TODO: Set the small subtext message
-         builder.setSubText("Tap to view");
+        builder.setSubText("Tap to view");
 
         // TODO: Set the content intent to launch our result activity
         builder.setContentIntent(pendingIntent);
 
 
         // TODO: Add an expanded layout to the notification
+        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+        bigTextStyle.setBigContentTitle("This is big notification");
+        bigTextStyle.bigText(getResources().getString(R.string.LongMsg));
+        builder.setStyle(bigTextStyle);
 
 
         // TODO: Add action buttons to the Notification if they are supported
         // Use the same PendingIntent as we use for the main notification action
-
+        builder.addAction(R.mipmap.ic_launcher, "Action 1", pendingIntent);
+        builder.addAction(R.mipmap.ic_launcher, "Action 2", pendingIntent);
         // TODO: Set the lock screen visibility of the notification
 
 
         // TODO: Build the finished notification and then display it to the user
         Notification notification = builder.build();
-        NotificationManager mgr = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        mgr.notify(NOTIFY_ID,notification);
+        NotificationManager mgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mgr.notify(NOTIFY_ID, notification);
 
     }
 
